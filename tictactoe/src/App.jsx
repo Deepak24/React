64e8704f6a3d81228975e837136d23c16815ab6a
DeepakK <khamkardeepak7@gmail.com>
2024-05-30 16:53:19 +0530

    Added tic tac toe react app file changes.

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+import './assets/css/styles.css';
+import Board from './components/Board';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h2>Learning React with Tic Tak Toe example</h2>
+        <Board />
+    </>
+  )
+}
+
+export default App;
