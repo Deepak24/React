404fd5cfdf0463d1a930d91d5e7e9878a7a0dfeb
DeepakK <khamkardeepak7@gmail.com>
2024-06-06 20:53:56 +0530

    Adding basic file changes.

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -3,14 +3,44 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+const strElement = <h3>We are displaying Element</h3>;
+
+function Header() {
+    return (
+        <div style={{ backgroundColor: 'gray', display: 'flex', flex: '1 0 auto' }}> 
+            <h2>Shopify App</h2>
+            <ul style={{ display: 'flex'}}>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+}
+
+function MyButton() {
+    return (
+        <>
+            <p>My Function component</p>
+            <button className=''>Submit</button>
+        </>
+    );
+}
+
 function App() {
     // const [count, setCount] = useState(0)
 
     return (
         <>
+            <Header />
             <h2>Hello Welcome to React!!</h2>
+            {
+                strElement
+            }
+            <MyButton />
+            
         </>
     )
 }
 
-export default App
+export default App;
