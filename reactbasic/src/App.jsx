0f5f203959e009cd6724dc25757b22c03d111cb0
DeepakK <khamkardeepak7@gmail.com>
2024-06-11 22:10:25 +0530

    Add CSS and main file changes.

--- reactbasic/src/App.jsx
+++ reactbasic/src/App.jsx
@@ -5,7 +5,9 @@ import './App.css'
 
 const Products = [{id: "1", item: "Shirt"},
                 {id: "2", item:"Short"},
-                {id: "3", item: "T-Shirt"}
+                {id: "3", item: "T-Shirt"},
+                {id: "4", item: "Jeans"},
+                {id: "5", item: "Formal-Pant"}
 ];
 
 const strElement = <h3>We are displaying Element</h3>;
@@ -39,9 +41,18 @@ function Loader() {
 }
 
 const isLoading = false;
+let count = 0;
 
 function App() {
     // const [count, setCount] = useState(0)
+    const onIncrementClick = () => {
+        count = count + 1;
+        console.log(count);
+    }
+
+    const onSearchChange = (event) => {
+        console.log(event.target.value);
+    }
 
     return (
         <>
@@ -56,8 +67,16 @@ function App() {
                 isLoading ? <h3>Page Loaded.</h3> : <Loader /> 
             }
 
-            <Products />
-            
+            <ul>
+            {//Product displaying 
+                Products.map( product => 
+                    <li className={`${product.id % 2 === 0 ? 'bgColor' : '' } padd8`} key={product.id} >{product.item}</li>
+                )
+            }
+            </ul>
+           
+            <input type="text" onChange={onSearchChange} placeholder='Search...' />
+            <button onClick ={onIncrementClick}>Button Increment</button>
         </>
     )
 }
